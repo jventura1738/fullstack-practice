15832abcc48d034bda53407a491187b14c30f5f8
Jacob <jacobduncan00@yahoo.com>
2021-09-06 23:31:46 -0400

    update

--- backend/src/website/__init__.py
+++ backend/src/website/__init__.py
@@ -1,10 +1,13 @@
 # Justin Ventura
 from flask import Flask
+from flask_cors import CORS
+
 
 
 # Creates app
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config['SECRET_KEY'] = 'jenna sleeps on the floor'
 
     from .views import views
